b69e6b0585930d6d4ca5d6050f36a331a199bcc4
Yongqi Pi <799817183@qq.com>
2025-02-05 17:53:10 -0500

    Dev (#6)
    * Change to using sidebar
    
    ---------
    
    Co-authored-by: Yongqi Pi <piy3@mcmaster.ca>

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
 import { Toaster } from "@/components/ui/toaster"
+import Page from "@/app/dashboard/page"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Page />
     <Toaster />
   </StrictMode>,
 )
